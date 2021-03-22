4d41d498b83543200b7d778e31dba057b4b020d4
tuonglucdt <tuongluc999@gmail.com>
2021-03-22 21:58:25 +0700

    Buoi 4-1

---
+++ TuongVanTienLuc_5951071055/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TuongVanTienLuc_5951071055.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
